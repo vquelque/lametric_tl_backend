a626b87a99eab531e7c9c0dedb4c64cd2e3321d5
Valentin Quelquejay <valentin.quelquejay-leclere@epfl.ch>
2019-11-11 19:33:04 +0100

    fix npm start in package.json

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require("express");
 
 const routes = require("./routes/tl_routes");
 const app = express();
-const port = 3000;
+const port = 8080;
 app.use("/", routes);
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
