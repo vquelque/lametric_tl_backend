77299e6e37e63bf2d88704646304b422e945ddd4
Valentin Quelquejay <valentin.quelquejay-leclere@epfl.ch>
2019-11-11 19:08:46 +0100

    add gitignore

--- routes/tl_routes.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var router = express.Router();
-var controller = require("../controllers/tl_controller");
-
-router.get("/departures", controller.getNextDepartures);
-
-module.exports = router;
--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-
-const routes = require("./routes/tl_routes");
-const app = express();
-const port = 3000;
-app.use("/", routes);
-app.listen(port, () => console.log(`Server listening on port ${port}!`));
