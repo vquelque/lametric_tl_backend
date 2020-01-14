b6ecf146dbcf95ed3edd908c356cf27921d4e697
Valentin Quelquejay <valentin.quelquejay-leclere@epfl.ch>
2020-01-14 15:56:20 +0100

    server PORT and IP environment variables

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 
 const routes = require("./routes/tl_routes");
 const app = express();
-const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+const server_port = process.env.PORT || 8080;
+const server_ip_address = process.env.IP || "127.0.0.1";
 app.use("/", routes);
 app.listen(server_port, server_ip_address, () =>
   console.log(`Server listening on port ${server_ip_address}:${server_port}!`)
